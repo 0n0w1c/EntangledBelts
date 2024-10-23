674c56f13395d3ce7be07eff4f89aa43304d7397
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2024-10-22 19:39:41 -0500

    Version 2.0.1

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -8,9 +8,12 @@ local function clone_entity(clone_name, original, upgrade)
     return clone
 end
 
+local express_upgrade = nil
+if mods["space-age"] then express_upgrade = "turbo-entangled-belt" end
+
 local entangled_belt = clone_entity("entangled-belt", "underground-belt", "fast-entangled-belt")
 local fast_entangled_belt = clone_entity("fast-entangled-belt", "fast-underground-belt", "express-entangled-belt")
-local express_entangled_belt = clone_entity("express-entangled-belt", "express-underground-belt", nil)
+local express_entangled_belt = clone_entity("express-entangled-belt", "express-underground-belt", express_upgrade)
 
 if entangled_belt and fast_entangled_belt and express_entangled_belt then
     data:extend { entangled_belt, fast_entangled_belt, express_entangled_belt }
