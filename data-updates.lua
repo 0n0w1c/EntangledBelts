c9728de793d0e867bac63f846eb9e79d2593e593
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-01-04 05:15:12 -0600

    Version 2.1.1

--- data-updates.lua
+++ data-updates.lua
@@ -4,4 +4,5 @@ data.raw["item"]["express-underground-belt"].subgroup = "belt-underground"
 
 if mods["space-age"] then
     data.raw["item"]["turbo-underground-belt"].subgroup = "belt-underground"
+    data.raw["recipe"]["turbo-entangled-belt"].surface_conditions = nil
 end
