d15eb5ceb87d8d7777ae5e0f5a9abf886d31c632
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-02-08 21:37:22 -0600

    Version 2.4.0

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,11 @@ require("prototypes.entities")
 require("prototypes.items")
 require("prototypes.recipes")
 require("prototypes.technology")
+
+if settings.startup["eb-no-red-inserter-challenge"] then
+    local challenge_accepted = settings.startup["eb-no-red-inserter-challenge"].value
+
+    data.raw["inserter"]["long-handed-inserter"].hidden = challenge_accepted
+    data.raw["item"]["long-handed-inserter"].hidden = challenge_accepted
+    data.raw["recipe"]["long-handed-inserter"].hidden = challenge_accepted
+end
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+      type = "bool-setting",
+      name = "eb-no-red-inserter-challenge",
+      setting_type = "startup",
+      default_value = false,
+      order = "a"
+    }
+})
