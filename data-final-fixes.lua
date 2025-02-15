2d438040694b7bbfc7d52f1139caf723e1b339f8
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-02-15 08:00:53 -0600

    Version 2.4.1

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,7 +6,10 @@ require("prototypes.technology")
 if settings.startup["eb-no-red-inserter-challenge"] then
     local challenge_accepted = settings.startup["eb-no-red-inserter-challenge"].value
 
-    data.raw["inserter"]["long-handed-inserter"].hidden = challenge_accepted
-    data.raw["item"]["long-handed-inserter"].hidden = challenge_accepted
-    data.raw["recipe"]["long-handed-inserter"].hidden = challenge_accepted
+    if challenge_accepted then
+        data.raw["inserter"]["long-handed-inserter"].hidden = challenge_accepted
+        data.raw["inserter"]["long-handed-inserter"].next_upgrade = nil
+        data.raw["item"]["long-handed-inserter"].hidden = challenge_accepted
+        data.raw["recipe"]["long-handed-inserter"].hidden = challenge_accepted
+    end
 end
